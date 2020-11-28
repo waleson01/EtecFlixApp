d75e909e0384196e5e5f8f7ea9a2158a5d27489a
waleson01 <waleson01@users.noreply.github.com>
2020-11-27 23:15:28 -0300

    .

---
+++ EtecFlixApp/EtecFlixApp/Filmes/VelozesFuriosos.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EtecFlixApp.Filmes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VelozesFuriosos : ContentPage
+    {
+        public VelozesFuriosos()
+        {
+            InitializeComponent();
+        }
+    }
+}
